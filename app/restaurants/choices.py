1b51520b2929add80c6e05af0da25eb9430b4ce0
nkstlrv <stlrvnk@gmail.com>
2023-07-12 12:30:46 +0300

    Dish model

---
+++ app/restaurants/choices.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class RestaurantCostChoices(models.IntegerChoices):
+    """
+    Choices for cost field in Restaurant model
+    """
+    LOW = 1, ('Low')
+    MID = 2, ('Mid')
+    HIGH = 2, ('High')
+    
+    
+class DishDayChoices(models.IntegerChoices):
+    """
+    Choices for day of the week
+    """
+    EVERYDAY = 0, ('Everyday')
+    MONDAY = 1, ('Monday')
+    TUESDAY = 2, ('Tuesday')
+    WEDNESDAY = 3, ('Wednesday')
+    THURSDAY = 4, ('Thursday')
+    Friday = 5, ('Friday')
+    SATURDAY = 6, ('Saturday')
+    SUNDAY = 7, ('Sunday')
--- app/restaurants/models.py
+++ app/restaurants/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-from .choises import RestaurantCostChoices
+from .choices import RestaurantCostChoices, DishDayChoices
+from .models import Dish
+from django.core.validators import MinValueValidator
 
 
 class Restaurant(models.Model):
     """
     Model to store main info about restaurants:
     - their name, address, cuisine, cost(low, mid, high)
+    - phone number 
+    - delivery option
     - menu (foreign key to Menu model)
     """
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=255)
     address = models.CharField(max_length=500)
-    cuisine = models.CharField(max_length=300, null=True, default=None)
+    phone_number = models.CharField(max_length=15)
+    cuisine = models.CharField(max_length=100, null=True, default=None)
     cost = models.CharField(choices=RestaurantCostChoices.choices, default=RestaurantCostChoices.MID)
+    delivery = models.BooleanField(default=False)
+    dish = models.OneToOneField(model=Dish, on_delete=models.CASCADE, primary_key=True)
     
     
     def __str__(self):
@@ -19,3 +26,18 @@ class Restaurant(models.Model):
     
 
 
+class Dish(models.Model):
+    """
+    Model to store available dishes and their day of availability
+    - day (week day when this dish is available)
+    - name (name of the dish)
+    - price (price of the dish)
+    """
+    name = models.CharField(max_length=150)
+    day = models.CharField(choices=DishDayChoices.choices, default=DishDayChoices.EVERYDAY)
+    price = models.DecimalField(max_digits=6, 
+                                decimal_places=2, 
+                                null=True, 
+                                default=None, 
+                                validators=[MinValueValidator(0)])
+    
