986b20d57c96839452ad097f8c4b179341be5ff1
nkstlrv <stlrvnk@gmail.com>
2023-07-12 14:03:12 +0300

    Dishes column to menu model

--- app/restaurants/admin.py
+++ app/restaurants/admin.py
@@ -24,11 +24,15 @@ class MenuAdmin(admin.ModelAdmin):
     list_display = [
         'restaurant',
         'get_day_display',
+        'dishes',
     ]
 
     list_filter = [
         'restaurant',
     ]
+    
+    def dishes(self, obj):
+        return " ,".join([str(i) for i in Dish.objects.filter(menu_id=obj.id)])
     
 
 
