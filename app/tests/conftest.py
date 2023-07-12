fe3383bc5f5eb471b65444d8be6241436aff3cb2
nkstlrv <stlrvnk@gmail.com>
2023-07-13 01:07:55 +0300

    authentication fixture

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,1 +1,19 @@
-# import pytest
+import pytest
+from rest_framework.test import APIClient
+
+
+client = APIClient()
+
+
+@pytest.fixture
+def authenticate_user():
+    """
+    With this fixture we can easily authenticate before testing endpoints
+    """
+    payload = dict(
+        username="TestUser",
+        password="TestPassword"
+    )
+
+    client.post("/api/register/", payload)
+    client.post("/api/login/", payload)
