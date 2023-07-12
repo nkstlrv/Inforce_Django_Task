2db1d2e039bf7f9716eddc13c3072674cbdc43bb
nkstlrv <stlrvnk@gmail.com>
2023-07-12 17:31:26 +0300

    temporary deleted auth app

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,7 +24,10 @@ urlpatterns = [
     # endpoint of restaurants app APIs
     path('api/v1/', include("restaurants.urls")),
     
-    # authentication app
-    path('api/v1/auth/', include("authentication.urls"))
+    # authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     
+
 ]
