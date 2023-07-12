59a5a5d6e9f0b34bd2a8e33f95a889d586d0ad3c
nkstlrv <stlrvnk@gmail.com>
2023-07-12 13:33:58 +0300

    registered new models to admin

--- app/restaurants/admin.py
+++ app/restaurants/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Restaurant, Dish
+from .models import Restaurant, Dish, Menu
 
 
-
-
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(Dish)
