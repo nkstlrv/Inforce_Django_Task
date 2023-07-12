fd4ae90fd1ce9224e73d9f03ca174e67cd02bd34
nkstlrv <stlrvnk@gmail.com>
2023-07-12 23:14:12 +0300

    menu admin

--- app/menu/admin.py
+++ app/menu/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Menu
 
-# Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['restaurant', 'get_day', 'get_dishes', 'get_votes_count']
+
+    def get_day(self, obj):
+        return dict(Menu.WEEKDAY_CHOICES)[obj.day]
+
+    def get_dishes(self, obj):
+        return ", ".join([dish.name for dish in obj.dishes.all()])
+
+    def get_votes_count(self, obj):
+        return obj.vote_set.count()
+
+    get_votes_count.short_description = 'Votes'
